d40f9b4eaf40366dbb9a9e3ac6a57b37087d6e66
Pratikchhya Shrestha <pat@tilde.team>
2022-10-05 22:11:31 -0500

    add title tests
    tests pass ![screenshot of visual studio showing tests pass](https://user-images.githubusercontent.com/56666486/194205559-f8a3341d-e13c-4f65-a72e-c2aaa947a94d.png)

---
+++ Texas.XunitTests/TitleTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Texas.XunitTests;
+
+public class TitleTests
+{
+    IConfiguration Configuration { get; set; }
+
+    public TitleTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<PersonTests>();
+
+        Configuration = builder.Build();
+    }
+
+    [Fact]
+    public void PreferredNameNull_ShouldSucceed()
+    {
+        // Arrange, act, assert
+        Title actual = new(null);
+        Title actualAnother = new(null);
+        actual.Should().Be(actualAnother);
+    }
+
+    [Theory]
+    [InlineData("Miss")]
+    [InlineData("Mr.")]
+    [InlineData("Esq.")]
+    [InlineData("ちゃん")]
+    public void LegalNameNotNull_ShouldSucceed(string legalName)
+    {
+        // Arrange, act, assert
+        Title actual = new(legalName);
+        Title actualAnother = new(legalName);
+        actual.Should().Be(actualAnother);
+    }
+}
