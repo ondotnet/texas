918958c7419f4cf0785ea4d4ea4b684959af08a5
Pratikchhya Shrestha <pat@tilde.team>
2022-10-05 22:05:26 -0500

    add preferred name tests

---
+++ Texas.XunitTests/PreferredNameTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Texas.XunitTests;
+
+public class PreferredNameTests
+{
+    IConfiguration Configuration { get; set; }
+
+    public PreferredNameTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<PersonTests>();
+
+        Configuration = builder.Build();
+    }
+
+    [Fact]
+    public void PreferredNameNull_ShouldSucceed()
+    {
+        // Arrange, act, assert
+        PreferredName actual = new(null);
+        PreferredName actualAnother = new(null);
+        actual.Should().Be(actualAnother);
+    }
+
+    [Theory]
+    [InlineData("Miss Theron")]
+    [InlineData("Barack Obama")]
+    [InlineData("Bill Clinton")]
+    [InlineData("愛")]
+    public void LegalNameNotNull_ShouldSucceed(string legalName)
+    {
+        // Arrange, act, assert
+        PreferredName actual = new(legalName);
+        PreferredName actualAnother = new(legalName);
+        actual.Should().Be(actualAnother);
+    }
+}
