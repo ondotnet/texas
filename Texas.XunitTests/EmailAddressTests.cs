497f7d4449162676f880082368d4a2c474a2105c
Pratikchhya Shrestha <pat@tilde.team>
2022-10-05 21:32:14 -0500

    add a simple test
    for email address

---
+++ Texas.XunitTests/EmailAddressTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Texas.XunitTests;
+
+public class EmailAddressTests
+{
+    IConfiguration Configuration { get; set; }
+
+    public EmailAddressTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<PersonTests>();
+
+        Configuration = builder.Build();
+    }
+
+    [Fact]
+    public void EmailAddressNull_ShouldThrowNullReferenceException()
+    {
+        // Arrange, Act, Assert
+        Action action = () => new EmailAddress(null);
+        action.Should().Throw<NullReferenceException>();
+    }
+}
