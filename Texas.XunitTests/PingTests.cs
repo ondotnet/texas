db3659a1caae721062e789f9bc1b809f50718077
Kushal Hada <kus@tilde.team>
2022-10-31 00:48:02 -0400

    wip

---
+++ Texas.XunitTests/PingTests.cs
@@ -0,0 +1,41 @@
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Texas.XunitTests;
+
+public class PingTests
+{
+    [Theory]
+    [InlineData("8.8.8.8")]
+    [InlineData("1.1.1.1")]
+    [InlineData("8.8.4.4")]
+    [InlineData("1.0.0.1")]
+    [InlineData("192.168.12.1")]
+    public void TestPing(string destination)
+    {
+        int totalCount = 20;
+        Ping pingSender = new Ping();
+        PingOptions options = new PingOptions();
+
+        // Use the default Ttl value which is 128,
+        // but change the fragmentation behavior.
+        options.DontFragment = true;
+
+        // Create a buffer of 32 bytes of data to be transmitted.
+        string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        byte[] buffer = Encoding.ASCII.GetBytes(data);
+        int timeout = 120;
+        PingReply reply = pingSender.Send(destination, timeout, buffer, options);
+        for (int i = 0; i < totalCount; i++)
+        {
+            if (reply?.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply?.Options?.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply?.Options?.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply?.Buffer.Length);
+            }
+        }
+    }
+}
